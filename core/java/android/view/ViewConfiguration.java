0ae5bb07b20667f9051bec6c034e941f7a3295f4
minaripenguin <minaripenguin@users.noreply.github.com>
2023-09-07 22:54:04 +0800

    ViewConfiguration: Disable fading marquee
    * its quite expensive even on modern gpus, tensor/gs series disables this feature by default, remove the depedency for config to prevent trees from overriding this.
    
    Change-Id: I23dd9763d48b8b172210e96b38fe0afd1b93d23c
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/view/ViewConfiguration.java
+++ core/java/android/view/ViewConfiguration.java
@@ -454,7 +454,7 @@ public class ViewConfiguration {
         mMaximumDrawingCacheSize = MAXIMUM_DRAWING_CACHE_SIZE;
         mOverscrollDistance = OVERSCROLL_DISTANCE;
         mOverflingDistance = OVERFLING_DISTANCE;
-        mFadingMarqueeEnabled = true;
+        mFadingMarqueeEnabled = false;
         mGlobalActionsKeyTimeout = GLOBAL_ACTIONS_KEY_TIMEOUT;
         mHorizontalScrollFactor = HORIZONTAL_SCROLL_FACTOR;
         mVerticalScrollFactor = VERTICAL_SCROLL_FACTOR;
@@ -547,7 +547,7 @@ public class ViewConfiguration {
             }
         }
 
-        mFadingMarqueeEnabled = res.getBoolean(R.bool.config_ui_enableFadingMarquee);
+        mFadingMarqueeEnabled = false;
         mTouchSlop = res.getDimensionPixelSize(R.dimen.config_viewConfigurationTouchSlop);
         mHandwritingSlop = res.getDimensionPixelSize(
                 R.dimen.config_viewConfigurationHandwritingSlop);
