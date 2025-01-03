f60a8f3fe6a260ce77c6a0fe97252dba04bcb2b5
Quick ⚡ <quick@disroot.org>
2025-01-03 06:25:07 +0530

    Update fp
    Change-Id: I091fa97337a4610fa0cb2bdc9e34b0c23f603256

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -73,14 +73,14 @@ public class PropImitationHooks {
             "com.google.android.gms/.auth.uiflows.minutemaid.MinuteMaidActivity");
 
     private static final Map<String, String> sPixelSixProps = Map.of(
-            "PRODUCT", "lynx",
-            "DEVICE", "lynx",
-            "HARDWARE", "lynx",
+            "PRODUCT", "raven",
+            "DEVICE", "raven",
+            "HARDWARE", "raven",
             "MANUFACTURER", "Google",
             "BRAND", "google",
-            "MODEL", "Pixel 7a",
-            "ID", "AP4A.241205.013.C1",
-            "FINGERPRINT", "google/lynx/lynx:15/AP4A.241205.013.C1/12657666:user/release-keys"
+            "MODEL", "Pixel 6 Pro",
+            "ID", "AP4A.241205.013.A2",
+            "FINGERPRINT", "google/raven/raven:15/AP4A.241205.013.A2/12649814:user/release-keys"
     );
 
     private static final Map<String, String> sPixelXLProps = Map.of(
