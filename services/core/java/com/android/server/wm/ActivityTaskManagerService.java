67585bec08f269559449a8659a6808623b669011
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-04-23 09:50:08 +0800

    services: boost thread priority for critical locks
    reverse-engineered from nothing os 3.0
    
    Change-Id: Ifeb825b44f9aa9eb5421b15fe810be946802a8b2
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/wm/ActivityTaskManagerService.java
+++ services/core/java/com/android/server/wm/ActivityTaskManagerService.java
@@ -2531,8 +2531,13 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
             return ParceledListSlice.emptyList();
         }
         mRecentTasks.loadRecentTasksIfNeeded(userId);
-        synchronized (mGlobalLock) {
-            return mRecentTasks.getRecentTasks(maxNum, flags, allowed, userId, callingUid);
+        WindowManagerService.boostPriorityForLockedSection();
+        try {
+            synchronized (mGlobalLock) {
+                return mRecentTasks.getRecentTasks(maxNum, flags, allowed, userId, callingUid);
+            }
+        } finally {
+            WindowManagerService.resetPriorityAfterLockedSection();
         }
     }
 
@@ -2541,11 +2546,13 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
         enforceTaskPermission("getAllRootTaskInfos()");
         final long ident = Binder.clearCallingIdentity();
         try {
+            WindowManagerService.boostPriorityForLockedSection();
             synchronized (mGlobalLock) {
                 return mRootWindowContainer.getAllRootTaskInfos(INVALID_DISPLAY);
             }
         } finally {
             Binder.restoreCallingIdentity(ident);
+            WindowManagerService.resetPriorityAfterLockedSection();
         }
     }
 
@@ -3969,6 +3976,7 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
         enforceTaskPermission("cancelTaskWindowTransition()");
         final long ident = Binder.clearCallingIdentity();
         try {
+            WindowManagerService.boostPriorityForLockedSection();
             synchronized (mGlobalLock) {
                 final Task task = mRootWindowContainer.anyTaskForId(taskId,
                         MATCH_ATTACHED_TASK_ONLY);
@@ -3980,6 +3988,7 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
             }
         } finally {
             Binder.restoreCallingIdentity(ident);
+            WindowManagerService.resetPriorityAfterLockedSection();
         }
     }
 
