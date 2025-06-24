00168129da1182cc09bdbc9d2e18cd6e1d1ff340
Quick ⚡ <quick@disroot.org>
2025-06-24 22:23:04 +0530

    Revert "Reparent surface to original parent post-rotation animation"
    This reverts commit 35aedb70166c670385c5626da3fdd34aa6d32ec9.

--- libs/WindowManager/Shell/src/com/android/wm/shell/transition/ScreenRotationAnimation.java
+++ libs/WindowManager/Shell/src/com/android/wm/shell/transition/ScreenRotationAnimation.java
@@ -79,7 +79,6 @@ class ScreenRotationAnimation {
     private final float[] mTmpFloats = new float[9];
     /** The leash of the changing window container. */
     private final SurfaceControl mSurfaceControl;
-    private final SurfaceControl mRootLeash;
 
     private final int mAnimHint;
     private final int mStartWidth;
@@ -130,7 +129,6 @@ class ScreenRotationAnimation {
         mEndHeight = change.getEndAbsBounds().height();
         mStartRotation = change.getStartRotation();
         mEndRotation = change.getEndRotation();
-        mRootLeash = rootLeash;
 
         mAnimLeash = new SurfaceControl.Builder()
                 .setParent(rootLeash)
@@ -392,7 +390,6 @@ class ScreenRotationAnimation {
             t.remove(mBackColorSurface);
         }
         if (mBackEffectSurface != null && mBackEffectSurface.isValid()) {
-            t.reparent(mSurfaceControl, mRootLeash);
             t.remove(mBackEffectSurface);
         }
         t.apply();
