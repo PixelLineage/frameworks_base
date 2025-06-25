cfdd1e54d1fa01aed24db69245663d00a0667b42
Quick ⚡ <quick@disroot.org>
2025-06-25 23:44:16 +0530

    Revert "fw/b: Add support for runtime toggle of navbar"
    This reverts commit 2f15b375fee7b1106ea28b32bfdb4f58c77eb7bc.

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -1194,8 +1194,6 @@ public final class PowerManagerService extends SystemService
     private static native boolean nativeSetPowerMode(int mode, boolean enabled);
     private static native boolean nativeForceSuspend();
 
-    private boolean mForceNavbar;
-
     // Whether proximity check on wake is enabled by default
     private boolean mProximityWakeEnabledByDefaultConfig;
 
@@ -1557,9 +1555,6 @@ public final class PowerManagerService extends SystemService
         resolver.registerContentObserver(LineageSettings.Secure.getUriFor(
                 LineageSettings.Secure.KEYBOARD_BRIGHTNESS),
                 false, mSettingsObserver, UserHandle.USER_ALL);
-        resolver.registerContentObserver(LineageSettings.System.getUriFor(
-                LineageSettings.System.FORCE_SHOW_NAVBAR),
-                false, mSettingsObserver, UserHandle.USER_ALL);
 
         // Register for broadcasts from other components of the system.
         IntentFilter filter = new IntentFilter();
@@ -1709,10 +1704,6 @@ public final class PowerManagerService extends SystemService
         mKeyboardBrightness = LineageSettings.Secure.getFloatForUser(resolver,
                 LineageSettings.Secure.KEYBOARD_BRIGHTNESS, mKeyboardBrightnessDefault,
                 UserHandle.USER_CURRENT);
-
-        mForceNavbar = LineageSettings.System.getIntForUser(resolver,
-                LineageSettings.System.FORCE_SHOW_NAVBAR,
-                0, UserHandle.USER_CURRENT) == 1;
 
         mDirty |= DIRTY_SETTINGS;
     }
@@ -3143,17 +3134,15 @@ public final class PowerManagerService extends SystemService
                         if (wakefulness == WAKEFULNESS_AWAKE) {
                             if (mButtonsLight != null) {
                                 float buttonBrightness = BRIGHTNESS_OFF_FLOAT;
-                                if (!mForceNavbar) {
-                                    if (isValidBrightness(
-                                            mButtonBrightnessOverrideFromWindowManager)) {
-                                        if (mButtonBrightnessOverrideFromWindowManager >
-                                                PowerManager.BRIGHTNESS_MIN) {
-                                            buttonBrightness =
-                                                    mButtonBrightnessOverrideFromWindowManager;
-                                        }
-                                    } else if (isValidButtonBrightness(mButtonBrightness)) {
-                                        buttonBrightness = mButtonBrightness;
+                                if (isValidBrightness(
+                                        mButtonBrightnessOverrideFromWindowManager)) {
+                                    if (mButtonBrightnessOverrideFromWindowManager >
+                                            PowerManager.BRIGHTNESS_MIN) {
+                                        buttonBrightness =
+                                                mButtonBrightnessOverrideFromWindowManager;
                                     }
+                                } else if (isValidButtonBrightness(mButtonBrightness)) {
+                                    buttonBrightness = mButtonBrightness;
                                 }
 
                                 if (!mButtonLightOnKeypressOnly) {
